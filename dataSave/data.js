f314f6db0dd5083383434e3e34b8c7bbd53f5320
871246995 <871246995@qq.com>
2016-06-20 23:27:55 +0800

    20160620

--- dataSave/data.js
+++ dataSave/data.js
@@ -4,11 +4,24 @@ var args = process.argv.splice(2);
 var command = args.shift();
 var taskDescription = args.join(' ');
 var file = path.join(process.cwd(), '/.tasks');
-console.log('argss=====' + process.argv);
-console.log('argss0=====' + process.argv[0]);
-console.log('argss1=====' + process.argv[1]);
-console.log('argss2=====' + process.argv[2]);
-console.log('args=====' + args);
-console.log('command=====' + command);
-console.log('taskDescription=====' + taskDescription);
-console.log('file=====' + file);
+// console.log('argss=====' + process.argv);
+// console.log('argss0=====' + process.argv[0]);
+// console.log('argss1=====' + process.argv[1]);
+// console.log('argss2=====' + process.argv[2]);
+// console.log('args=====' + args);
+// console.log('command=====' + command);
+// console.log('taskDescription=====' + taskDescription);
+// console.log('file=====' + file);
+switch(command){
+	case 'list':
+	listTasks(file);
+	break;
+
+	case 'add' :
+	addTask(file, taskDescription);
+	break;
+
+	default:
+	console.log('Usage: ' + process.argv[0]
+		+ ' list|add [taskDescription]');
+}
