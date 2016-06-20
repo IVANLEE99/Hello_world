ea1a2844fbc9982a9b116ab17967f63ffe0d2ef2
IVANLEE99 <871246995@qq.com>
2016-06-20 11:37:41 +0800

    20160620

---
+++ dataSave/data.js
@@ -0,0 +1,14 @@
+var fs = require ('fs');
+var path = require ('path');
+var args = process.argv.splice(2);
+var command = args.shift();
+var taskDescription = args.join(' ');
+var file = path.join(process.cwd(), '/.tasks');
+console.log('argss=====' + process.argv);
+console.log('argss0=====' + process.argv[0]);
+console.log('argss1=====' + process.argv[1]);
+console.log('argss2=====' + process.argv[2]);
+console.log('args=====' + args);
+console.log('command=====' + command);
+console.log('taskDescription=====' + taskDescription);
+console.log('file=====' + file);
